b28aefe9c761e1daba4b45c4375f5cee467f4d07
stjepanstojcevic <stjepanstojcevic@gmail.com>
2023-10-30 16:40:03 +0100

    Initial Commit

---
+++ PokemonKingdom/PokemonKingdomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonKingdomApp.swift
+//  PokemonKingdom
+//
+//  Created by Stjepan Stojčević on 30.10.2023..
+//
+
+import SwiftUI
+
+@main
+struct PokemonKingdomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
